ed0d6e6eb26fd8d3e4bc68352bd6f1b8d557e6ba
Rishitha Bura <rishithabura@gmail.com>
2024-01-28 18:59:10 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,5 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+//Author: Rishitha Bura <rishithabura@gmail.com> 
 // Header file for input output functions 
 #include<iostream>  
   
